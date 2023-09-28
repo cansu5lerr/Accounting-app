6570e85ecd729b18f7be602c6b82afe3212f312f
Cansu Beşler <83400245+cansu5lerr@users.noreply.github.com>
2023-09-28 13:50:16 +0300

    Update chart.js

--- accounting-ui/src/components/chart.js
+++ accounting-ui/src/components/chart.js
@@ -1,24 +1,13 @@
-// import React from 'react';
-// import ApexCharts from 'react-apexcharts';
 
-// const Chart = ({ options, series }) => {
-//   return (
-//     <ApexCharts options={options} series={series} type="bar" height={350} />
-//   );
-// };
-
-// export default Chart;
 import React from 'react';
 import ApexCharts from 'react-apexcharts';
 
 const Chart = ({ options, series, onColumnClick }) => {
   const handleColumnClick = (event, chartContext, config) => {
-    // Bu fonksiyon sütuna tıklandığında çağrılacaktır.
-    // config verileri tıklanan sütunun bilgilerini içerecektir.
-    // İstediğiniz işlemi burada gerçekleştirebilirsiniz.
+
     
     if (onColumnClick) {
-      onColumnClick(config.dataPointIndex); // Tıklanan sütunun veri noktasını iletebilirsiniz
+      onColumnClick(config.dataPointIndex); 
     }
      console.log(config.dataPointIndex);
   };
@@ -30,7 +19,7 @@ const Chart = ({ options, series, onColumnClick }) => {
       type="bar" 
       height={350} 
       events={{
-        dataPointSelection: handleColumnClick // Tıklama olayını dinlemek için events özelliğini kullanın
+        dataPointSelection: handleColumnClick 
       }}
       
     />
