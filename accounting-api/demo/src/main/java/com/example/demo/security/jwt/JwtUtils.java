3966899eb3b112f9f949712325ffe2ddb94ecd78
Cansu Beşler <83400245+cansu5lerr@users.noreply.github.com>
2023-09-28 14:56:53 +0300

    Update JwtUtils.java

--- accounting-api/demo/src/main/java/com/example/demo/security/jwt/JwtUtils.java
+++ accounting-api/demo/src/main/java/com/example/demo/security/jwt/JwtUtils.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-//import com.bezkoder.spring.security.postgresql.security.services.UserDetailsImpl;
+
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
